3eafba7c7261057dd114c580a929dee91b715983
Krutartha <krutartha2002@gmail.com>
2020-01-19 08:08:32 +0530

    node

--- node-backend/app.js
+++ node-backend/app.js
@@ -42,35 +42,31 @@ app.get("/:origin/:destination/:date", (req, res, next) => {
 
     // console.log(response.body.Quotes[0].MinPrice);
   });
+});
 
-  //   var options = {
-  //     method: "GET",
-  //     hostname: "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-  //     port: null,
-  //     path:
-  //       "/apiservices/browsequotes/v1.0/US/INR/en-US/" +
-  //       origin +
-  //       "/" +
-  //       destination +
-  //       "/" +
-  //       date,
-  //     headers: {
-  //       "x-rapidapi-host":
-  //         "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-  //       "x-rapidapi-key": "3ca473d7d8msh05da92b5f33d66bp14d5dejsn2cbb731181c0"
-  //     }
-  //   };
-  //   var req = http.request(options, function(res) {
-  //     var chunks = [];
-  //     res.on("data", function(chunk) {
-  //       chunks.push(chunk);
-  //     });
-  //     res.on("end", function() {
-  //       var body = Buffer.concat(chunks);
-  //       responseFrom = body.toString();
-  //       console.log(JSON.stringify(body.toString()));
-  //     });
-  //   });
-  //   req.end();
-  //   res.json(responseFrom);
+app.get("monument/:coordinatex/:coordinatey/:coordinatez", (req, res, next) => {
+  var coordinatex = [req.params.coordinatex];
+  var coordinatey = [req.params.coordinatey];
+  var coordinatez = [req.params.coordinatez];
+
+  var unirest = require("unirest");
+
+  var req = unirest(
+    "GET",
+    "https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=0Frq5aaFxs2HPzEYhJbmrH_6OsjFpkpReqG13U4IG5k&mode=retrieveLandmarks&prox=" +
+      coordinatex +
+      "," +
+      coordinatey +
+      "," +
+      coordinatez
+  );
+
+  req.end(function(response) {
+    if (response.error) throw new Error(response.error);
+    console.log(response.body);
+
+    // res.json(responseFrom);
+
+    // console.log(response.body.Quotes[0].MinPrice);
+  });
 });
